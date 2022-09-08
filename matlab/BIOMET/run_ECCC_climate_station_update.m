e189d7eb58820acca3a5f5841a3131d87d84bd41
znesic <z_nesic@hotmail.com>
2022-09-07 19:04:04 -0700

    Added Hogg ECCC site to run_ECC_climate station_update. Added new function db_Young_climate_station.m

--- matlab/BIOMET/run_ECCC_climate_station_update.m
+++ matlab/BIOMET/run_ECCC_climate_station_update.m
@@ -1,21 +1,48 @@
-function run_ECCC_climate_station_update(yearsIn,monthsIn)
-% run_ECCC_climate_station_update(yearsIn,monthsIn)
+function run_ECCC_climate_station_update(yearsIn,monthsIn,stationIDs)
+% run_ECCC_climate_station_update(yearsIn,monthsIn,stationIDs)
 %
-% Process ECCC climate station data. The station ID numbers are currently hard
-% codded (49088)
+% Process ECCC climate station data. 
 %
 % yearsIn   - range of years to process (default current year)
 % monthsIn  - range of months to process (default previous and the current
 %             month.
+% stationIDs    - a vector of station IDs. They can be found here:
+%                 https://drive.google.com/drive/folders/1WJCDEU34c60IfOnG4rv5EPZ4IhhW9vZH
+%                 File: "Station Inventory EN.csv"
 %
 % Zoran Nesic                   File created:               2022
-%                               Last modification: July 14, 2022
+%                               Last modification:  Sep  7, 2022
 
 % Revisions
 %
+% Sep 7, 2022 (Zoran)
+%  - added input option for stationIDs
+%  - added calls for Manitoba station
 
 [yearNow,monthNow,~]= datevec(now);
 monthRange = monthNow-1:monthNow;
 arg_default('yearsIn',yearNow)
 arg_default('monthsIn',monthRange)
-db_ECCC_climate_station(yearsIn,monthsIn,49088,fullfile(biomet_database_default,'yyyy\BB\MET\ECCC'),60);
+arg_default('stationIDs',49088)
+
+for cntStations = 1:length(stationIDs)
+    sID = stationIDs(cntStations);
+    switch sID
+        case 49088
+            % Burns Bog station
+            pathECCC = 'yyyy\BB\Met\ECCC';
+        case 10927
+            % Hogg station
+            pathECCC = 'yyyy\Hogg\Met\ECCC';
+        otherwise
+            fprintf('station ID out of range!\n');     
+            pathECCC = '';
+    end
+    if ~isempty(pathECCC)
+        try
+            db_ECCC_climate_station(yearsIn,monthsIn,sID,...
+                                    fullfile(biomet_database_default,pathECCC),60);
+        catch
+        end
+    end
+end
