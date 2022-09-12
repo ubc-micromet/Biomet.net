0f665dfaa4ce8b93c38ba862e6dd0a96744b6c1b
znesic <z_nesic@hotmail.com>
2022-09-12 11:55:39 -0700

    Fixed up spelling of UBC_PC_Setup. 'S' should be CAPS

--- matlab/BIOMET/change_path_to_UBC_PC_setup.m
+++ matlab/BIOMET/change_path_to_UBC_PC_setup.m
@@ -1,30 +1,32 @@
-function [newPath,oldPath] = change_path_to_UBC_PC_setup(newPathPat)
+function [newPath,oldPath] = change_path_to_UBC_PC_Setup(newPathPat)
 % Change the path to all UBC_PC_setup subfolders to newPath
 %
-% newPathPat - new path pattern to "UBC_PC_setup" folder instead of the current one.
-%              newPathPat has to contain string "UBC_PC_setup/PC_specific"
+% newPathPat - new path pattern to "UBC_PC_Setup" folder instead of the current one.
+%              newPathPat has to contain string "UBC_PC_Setup/PC_specific"
 %
 % Program searches through the current path to find the occurance of "PC_specific".
-% It then uses that as the reference point of the current path for UBC_PC_setup functions.
-% It replaces all the lines that contain "UBC_PC_setup\PC_specific\" 
-%                        with <newPathPat>\UBC_PC_setup\PC_specific\.
+% It then uses that as the reference point of the current path for UBC_PC_Setup functions.
+% It replaces all the lines that contain "UBC_PC_Setup\PC_specific\" 
+%                        with <newPathPat>\UBC_PC_Setup\PC_specific\.
 % 
 % Example:
 %    If the original path is:
-%       c:\UBC_PC_setup
+%       c:\UBC_PC_Setup
 %    and the newPath is:
-%       D:\NZ\MATLAB\CurrentProjects\myVersion\UBC_PC_setup
+%       D:\NZ\MATLAB\CurrentProjects\myVersion\UBC_PC_Setup
 %    then all paths that contain:
 %       PC_Specific\
 %    will change to:
-%       D:\NZ\MATLAB\CurrentProjects\myVersion\UBC_PC_setup\PC_specific\
+%       D:\NZ\MATLAB\CurrentProjects\myVersion\UBC_PC_Setup\PC_specific\
 %
 % Zoran Nesic               File created:           Sep 11, 2022
-%                           Last modification:      Sep 11, 2022
+%                           Last modification:      Sep 12, 2022
 
 %
 % Revisions:
 %
+% Sep 12, 2022 (Zoran)
+%   - Decided that the proper spelling is UBC_PC_Setup (capital S!)
 %
 
 % Store the current full path
@@ -38,7 +40,7 @@ if ~strcmp(newPathPat(end),filesep)
 end
 
 % at this point newPathPat has to end with matlab\ or matlab/
-if ~strcmpi(newPathPat(end-12:end),['UBC_PC_setup' filesep])
+if ~strcmpi(newPathPat(end-12:end),['UBC_PC_Setup' filesep])
     error('Input path: %s has to end with the string "UBC_PC_setup"\nMaybe you want: %s%s%s',newPathPat,newPathPat,'UBC_PC_Setup',filesep)
 end
 % find the target folder (here we use the first occurance of 
