2a70c0591b69bfedaa8ff56787b5fb81a7eac730
znesic <z_nesic@hotmail.com>
2022-11-06 11:32:32 -0800

    Changed yearsToProcess handling. Added some comments and cleaned some lines

--- R/database_functions/Run_ThirdStage_REddyProc.R
+++ R/database_functions/Run_ThirdStage_REddyProc.R
@@ -2,15 +2,20 @@
 # By Sara Knox
 # Aug 11, 2022
 # 
-# Last modification: Nov 2, 2022 (Zoran)
+# Last modification: Nov 6, 2022 (Zoran)
 #
 # Example (it should be entered as one line if a Win CMD prompt) : 
-#   "C:\Program Files\R\R-4.2.1\bin\Rscript.exe" "C:\Biomet.net\R\database_functions\Run_ThirdStage_REddyProc.R" "C:\Biomet.net\R\database_functions" "E:\Junk\database\Calculation_procedures\TraceAnalysis_ini\DSM\log\DSM_ThirdStageCleaningParameters.ini"  2> "E:\Junk\database\Calculation_procedures\TraceAnalysis_ini\DSM\log\DSM_ThirdStageCleaning.log" 1>&2
-
+#   "C:\Program Files\R\R-4.2.1\bin\Rscript.exe" "C:/Biomet.net/R/database_functions/Run_ThirdStage_REddyProc.R" "C:/Biomet.net/R/database_functions" "p:\database\Calculation_Procedures\TraceAnalysis_ini\DSM\log\DSM_setThirdStageCleaningParameters.R"  2> "p:/database/Calculation_Procedures/TraceAnalysis_ini/DSM/log/DSM_ThirdStageCleaning.log" 1>&2
+# Calling it from R-Studio:
+#   args <- c("C:/Biomet.net/R/database_functions", "p:/database/Calculation_procedures/TraceAnalysis_ini/DSM/log/DSM_setThirdStageCleaningParameters.R")
+#   source("C:/Biomet.net/R/database_functions/Run_ThirdStage_REddyProc.R")
 
 
 # Revisions
 #
+# Nov 6, 2022 (Zoran)
+#   - All input parameters for REddyProc now come via an R script. 
+#     The path to this script is in:pathSetIni (the second argument to the function call)
 # Nov 2, 2022 (Zoran)
 #   - changed to work with most of input arguments being passed 
 #     via siteID_ThirdStageCleaningParamenters.ini. This file is usualy created
@@ -24,7 +29,7 @@
 if(length(commandArgs(trailingOnly = TRUE))==0){
     cat("\nIn: Run_ThirdStage_REddyProc:\nNo input parameters!\nUsing whatever is in args variable \n")
 } else {
-    # otherwise use the third argument to pass the path
+    # otherwise set args to commandArgs()
     args 		<- commandArgs(trailingOnly = TRUE)
 }    
 
@@ -37,8 +42,6 @@ source(pathSetIni)
 source(file.path(fx_path,'ThirdStage_REddyProc.R'))
 
 # Call third stage processing
-# ThirdStage_REddyProc(site, years, db_ini, db_out, ini_path, fx_path,Ustar_scenario,yearsToProcess,do_REddyProc)
-#ThirdStage_REddyProc(pathBiometR,pathInputArgs)
 ThirdStage_REddyProc(pathSetIni)
 
 cat("=============================================================================\n")
@@ -46,24 +49,8 @@ cat(" Warnings\n")
 cat("=============================================================================\n")
 warnings()
 cat("=============================================================================\n\n")
-
 cat("End of Run_ThirdStageREddyProc run.\n\n")
 
 
 
-
-
-#==========================
-# Old comments (still useful)
-#
-# input parameters
-# site <- 'DSM'
-# years <- 2022 # if using multiple years use c(year1,year2) 
-
-# ----------- could be made generic ---------------
-# define main paths
-#db_root <- "p:/database"
-#fx_path <- "C:/Ubc_flux/R/database_functions"   # Specify path for loading functions
-
-# ZORAN, some of these inputs edited to better work with the biomet.net flow.     
 
