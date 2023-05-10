6a1a9c4743b0469a072f30d1b3a62f08623b54c0
znesic <z_nesic@hotmail.com>
2023-05-10 17:47:02 +0000

    Fixing bugs in load_trace_from_database

--- matlab/SoilChambers/process_Picarro_AGGP_data.m
+++ matlab/SoilChambers/process_Picarro_AGGP_data.m
@@ -2,7 +2,7 @@ try
     fprintf('Started: %s\n',mfilename);
 %     diarylog
     disp(datestr(now));
-    cd \\teamshare.ead.ubc.ca\team\LFS\Research_Groups\Sean_Smukler\SALdata\matlab\Picarro_Beta_version
+    cd T:\Research_Groups\Sean_Smukler\SALdata\matlab\Picarro_Beta_version
     run_picarro_ACS_daily(now-2:now-1)
     disp(datestr(now));
 catch
