63834f7cf42f8092ffdecf9a38e72ef41d6c5adf
znesic <z_nesic@hotmail.com>
2023-07-26 15:42:02 -0700

    edited fr_valid_file_name for MacOS compatibility

--- matlab/NEW_MET/fr_valid_path_name.m
+++ matlab/NEW_MET/fr_valid_path_name.m
@@ -5,15 +5,17 @@ function pth_out = fr_valid_path_name(pth)
 % '\' at the end if path is OK, else return an empty matrix
 %
 % (c) Zoran Nesic       File created:       Feb 22, 1998
-%				        Last modification:	Feb 22, 1998
+%				        Last modification:	Jul 26, 2023
 %
 
 %
 % Revisions:
 %
+% July 26, 2023 (Zoran)
+%   - replaced '\' with filesep for MacOS compatibility
 
-if pth(length(pth)) ~= '\'          % path name must end with a '\'
-    pth = [pth '\'];
+if pth(length(pth)) ~= filesep          % path name must end with a '\'
+    pth = [pth filesep];
 end
 
 if exist(pth) == 7                  % check if the path exists (must be a
