19e3ecd1a5e0bd2ba204d746c7bbebf09990b710
znesic <z_nesic@hotmail.com>
2023-08-09 16:06:44 -0700

    Added some error handling in fr_HOBO_database

--- matlab/New_eddy/fr_HOBO_database.m
+++ matlab/New_eddy/fr_HOBO_database.m
@@ -22,10 +22,13 @@ function [numOfFilesProcessed,numOfDataPointsProcessed] = fr_HOBO_database(wildC
 %                     consecutive data points). Default 30 (hhour)
 %
 % Zoran Nesic           File Created:      Aug   7, 2023
-%                       Last modification: Aug   7, 2023
+%                       Last modification: Aug   9, 2023
 
 %
 % Revisions:
+%
+% Aug 9, 2023 (Zoran)
+%   - Added proper handling of the "No files found" for the given path
 %
 
 arg_default('time_shift',0);
@@ -38,98 +41,105 @@ else
     flagUseProgressList = 1;
 end
 h = dir(wildCardPath);
-x = strfind(wildCardPath,filesep);
-y = strfind(wildCardPath,'.*');
 
-pth = wildCardPath(1:x(end));
+if ~isempty(h)
+    x = strfind(wildCardPath,filesep);
+    y = strfind(wildCardPath,'.*');
 
-if flagUseProgressList == 1
-    if exist(processProgressListPath) %#ok<*EXIST> * do not use 'var' option  here. It does not work correctly
-        load(processProgressListPath,'filesProcessProgressList');
-    else
-        filesProcessProgressList = [];
-    end
+    pth = wildCardPath(1:x(end));
 
-    filesToProcess = [];                %#ok<*NASGU> % list of files that have not been processed or
-                                        % that have been modified since the last processing
-    indFilesToProcess = [];             % index of the file that needs to be process in the 
-                                        % filesProcessProgressList
-end
-
-numOfFilesProcessed = 0;
-numOfDataPointsProcessed = 0;
-warning_state = warning;
-warning('off') %#ok<*WNOFF>
-hnd_wait = waitbar(0,'Updating database...');
-
-for i=1:length(h)
-    fileName = fullfile(pth,h(i).name);
-    try 
-        waitbar(i/length(h),hnd_wait,sprintf('Processing: %s ', fileName))
-    catch  %#ok<*CTCH>
-        waitbar(i/length(h),hnd_wait)
-    end
-    flagProcessCurrentFile = 0;
     if flagUseProgressList == 1
-        % Find the current file in the fileProcessProgressList
-        j = findFileInProgressList(h(i).name, filesProcessProgressList);
-        % if it doesn't exist add a new value
-        if j > length(filesProcessProgressList)
-            filesProcessProgressList(j).Name = h(i).name; %#ok<*AGROW>
-            filesProcessProgressList(j).Modified = 0;      % datenum(h(i).date);
+        if exist(processProgressListPath) %#ok<*EXIST> * do not use 'var' option  here. It does not work correctly
+            load(processProgressListPath,'filesProcessProgressList');
+        else
+            filesProcessProgressList = [];
         end
+
+        filesToProcess = [];                %#ok<*NASGU> % list of files that have not been processed or
+                                            % that have been modified since the last processing
+        indFilesToProcess = [];             % index of the file that needs to be process in the 
+                                            % filesProcessProgressList
     end
-        
-    % if the file modification data change since the last processing  or if ProgressList is not used
-    % then reprocess it
-    if flagProcessCurrentFile == 0 ...
-            || (flagProcessCurrentFile== 1 && filesProcessProgressList(j).Modified < datenum(h(i).date)) 
-        try
-            % when a file is found that hasn't been processed
-            % load it using fr_read_HOBO_file
-            [~, ~,tv] = fr_read_HOBO_file(fileName,'caller','Stats');
-            tv = tv + time_shift;
 
-            % if there were no errors try to update database
-            % Save data belonging to different years to different folders
-            % if databasePath contains "\yyyy\" string (replace it with
-            % \2005\ for year 2005)
-            yearVector = datevec(tv);
-            yearVector = yearVector(:,1);
-            years = unique(yearVector)';
-            ind_yyyy = strfind(databasePath,[filesep 'yyyy' filesep]);
-            if ~isempty(ind_yyyy)            
-                databasePathNew = databasePath;
-                for year_ind = years
-                    one_year_ind = find(tv > datenum(year_ind,1,1) & tv <= datenum(year_ind+1,1,1));
-                    databasePathNew(ind_yyyy+1:ind_yyyy+4) = num2str(year_ind);
-                    [k] = db_new_eddy(Stats(one_year_ind),[],databasePathNew,0,[],timeUnit,missingPointValue); %#ok<*FNDSB>
+    numOfFilesProcessed = 0;
+    numOfDataPointsProcessed = 0;
+    warning_state = warning;
+    warning('off') %#ok<*WNOFF>
+    hnd_wait = waitbar(0,'Updating database...');
+
+    for i=1:length(h)
+        fileName = fullfile(pth,h(i).name);
+        try 
+            waitbar(i/length(h),hnd_wait,sprintf('Processing: %s ', fileName))
+        catch  %#ok<*CTCH>
+            waitbar(i/length(h),hnd_wait)
+        end
+        flagProcessCurrentFile = 0;
+        if flagUseProgressList == 1
+            % Find the current file in the fileProcessProgressList
+            j = findFileInProgressList(h(i).name, filesProcessProgressList);
+            % if it doesn't exist add a new value
+            if j > length(filesProcessProgressList)
+                filesProcessProgressList(j).Name = h(i).name; %#ok<*AGROW>
+                filesProcessProgressList(j).Modified = 0;      % datenum(h(i).date);
+            end
+        end
+
+        % if the file modification data change since the last processing  or if ProgressList is not used
+        % then reprocess it
+        if flagProcessCurrentFile == 0 ...
+                || (flagProcessCurrentFile== 1 && filesProcessProgressList(j).Modified < datenum(h(i).date)) 
+            try
+                % when a file is found that hasn't been processed
+                % load it using fr_read_HOBO_file
+                [~, ~,tv] = fr_read_HOBO_file(fileName,'caller','Stats');
+                tv = tv + time_shift;
+
+                % if there were no errors try to update database
+                % Save data belonging to different years to different folders
+                % if databasePath contains "\yyyy\" string (replace it with
+                % \2005\ for year 2005)
+                yearVector = datevec(tv);
+                yearVector = yearVector(:,1);
+                years = unique(yearVector)';
+                ind_yyyy = strfind(databasePath,[filesep 'yyyy' filesep]);
+                if ~isempty(ind_yyyy)            
+                    databasePathNew = databasePath;
+                    for year_ind = years
+                        one_year_ind = find(tv > datenum(year_ind,1,1) & tv <= datenum(year_ind+1,1,1));
+                        databasePathNew(ind_yyyy+1:ind_yyyy+4) = num2str(year_ind);
+                        [k] = db_new_eddy(Stats(one_year_ind),[],databasePathNew,0,[],timeUnit,missingPointValue); %#ok<*FNDSB>
+                    end
+                else
+                    [k] = db_new_eddy(Stats,[],databasePath,0,[],timeUnit,missingPointValue);
                 end
-            else
-                [k] = db_new_eddy(Stats,[],databasePath,0,[],timeUnit,missingPointValue);
-            end
-            % if there is no errors update records
-            numOfFilesProcessed = numOfFilesProcessed + 1;
-            numOfDataPointsProcessed = numOfDataPointsProcessed + length(tv);
-            if flagProcessCurrentFile == 1
-                filesProcessProgressList(j).Modified = datenum(h(i).date);
-            end
-        catch
-            fprintf('Error in processing of: %s\n',fullfile(pth,h(i).name))
-        end % of try
-    end %  if filesProcessProgressList(j).Modified < datenum(h(i).date)
-end % for i=1:length(h)
-% Close progress bar
-close(hnd_wait)
-% Return warning state 
-try  %#ok<TRYNC>
-   for i = 1:length(warning_state)
-      warning(warning_state(i).identifier,warning_state(i).state)
-   end
-end
+                % if there is no errors update records
+                numOfFilesProcessed = numOfFilesProcessed + 1;
+                numOfDataPointsProcessed = numOfDataPointsProcessed + length(tv);
+                if flagProcessCurrentFile == 1
+                    filesProcessProgressList(j).Modified = datenum(h(i).date);
+                end
+            catch
+                fprintf('Error in processing of: %s\n',fullfile(pth,h(i).name))
+            end % of try
+        end %  if filesProcessProgressList(j).Modified < datenum(h(i).date)
+    end % for i=1:length(h)
+    % Close progress bar
+    close(hnd_wait)
+    % Return warning state 
+    try  %#ok<TRYNC>
+       for i = 1:length(warning_state)
+          warning(warning_state(i).identifier,warning_state(i).state)
+       end
+    end
 
-if flagProcessCurrentFile == 1
-    save(processProgressListPath,'filesProcessProgressList')
+    if flagProcessCurrentFile == 1
+        save(processProgressListPath,'filesProcessProgressList')
+    end
+else
+    fprintf('No files matching: %s are found\nExiting...\n',wildCardPath);
+    numOfFilesProcessed = 0;
+    numOfDataPointsProcessed = 0;
 end
 
 % this function returns and index pointing to where fileName is in the 
