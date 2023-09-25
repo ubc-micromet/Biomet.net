b4a715c9022226147e3d54b5f19707b24e765aa1
znesic <z_nesic@hotmail.com>
2023-09-25 13:35:28 -0800

    edits

--- matlab/Micromet/mergeMicrometDumpAndHF_folders.m
+++ matlab/Micromet/mergeMicrometDumpAndHF_folders.m
@@ -20,8 +20,8 @@ mergeSmartFluxUSBdata('BB2','2023_Flux')
 ManitobaSites = {'HOGG','YOUNG','OHM'};
 
 for cntSite = 1:length(ManitobaSites)
-    folderToCopy = ['P:\Sites\' char(ManitobaSites(cntSite)) '\HighFrequencyData'];
-    mergedDataFolder = ['\\137.82.55.154\highfreq\' char(ManitobaSites(cntSite))];
+    folderToCopy = ['P:\Sites\' char(ManitobaSites(cntSite)) '\HighFrequencyData\raw'];
+    mergedDataFolder = ['\\137.82.55.154\highfreq\' char(ManitobaSites(cntSite)) '\raw'];
     fprintf('Merging: %s with %s\n',folderToCopy,mergedDataFolder);        
     cmdStr = sprintf('robocopy %s %s /R:3 /W:3 /E /REG /NDL /NFL /NJH',folderToCopy,mergedDataFolder);
     system(cmdStr);
