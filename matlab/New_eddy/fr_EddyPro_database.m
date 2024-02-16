993023ff5014a9632ce07b4a31ff0f22413fd2ec
znesic <z_nesic@hotmail.com>
2024-02-16 12:15:09 -0800

    Major update for fr_read_EddyPro_file. New database processing function fr_EddyPro_database that replaces the old fr_SmartFlux_database. Orders of magnitude faster for big EddyPro files

---
+++ matlab/New_eddy/fr_EddyPro_database.m
@@ -0,0 +1,130 @@
+function [numOfFilesProcessed,numOfDataPointsProcessed] = fr_EddyPro_database(wildCardPath,processProgressListPath,databasePath,time_shift,timeUnit,missingPointValue)
+% fr_EddyPro_database - reads EddyPro full_output, _biomet_ or the summary files and puts data into data base
+% 
+% fr_EddyPro_database(wildCardPath,processProgressListPath,databasePath,time_shift,timeUnit,missingPointValue)
+%
+% Example:
+% [nFiles,nHHours]=fr_EddyPro_database('d:\Sites\HH\SmartFlux\*_EP-Summary.txt', ...
+%                                  '\\annex001\database\HH\2024\HH_EddyPro_progressList.mat','\\annex001\database\yyyy\');
+%       This updates or creates data base under \\annex001\database folder.
+%
+%
+%
+% NOTE:
+%       databasePath needs to include "\yyyy\" string if multiple years of
+%       data are going to be found in the wildCardPath folder!
+%
+% Inputs:
+%       wildCardPath            - full SmartFlux summary file name, including path. Wild cards accepted
+%       processProgressListPath - path where the progress list is kept
+%       databasePath            - path to output location  (*** see the note above ***)
+%       timeShift               - time offset to be added to the tv vector (in tv
+%                                 units, 0 if datebase is in GMT)
+%       timeUnit                -  minutes in the sample period (spacing between two
+%                                  consecutive data points). Default '30min' (hhour)
+%       missingPointValue       - Values that indicate missing data (default = NaN)
+%
+% Zoran Nesic                   File Created:      Feb  16, 2024
+%                               Last modification: Feb  16, 2024
+
+% Created based on fr_SmartFlux_database.m
+
+%
+% Revisions:
+%
+
+
+arg_default('time_shift',0);
+arg_default('timeUnit','30MIN'); %
+arg_default('missingPointValue',0); %   % default missing point code is 0
+
+flagRefreshChan = 0;
+
+h = dir(wildCardPath);
+x = strfind(wildCardPath,filesep);
+y = strfind(wildCardPath,'.*');
+
+pth = wildCardPath(1:x(end));
+
+if exist(processProgressListPath) %#ok<*EXIST> * do not use 'var' option  here. It does not work correctly
+    load(processProgressListPath,'filesProcessProgressList');
+else
+    filesProcessProgressList = [];
+end
+
+filesToProcess = [];                %#ok<*NASGU> % list of files that have not been processed or
+                                    % that have been modified since the last processing
+indFilesToProcess = [];             % index of the file that needs to be process in the 
+                                    % filesProcessProgressList
+numOfFilesProcessed = 0;
+numOfDataPointsProcessed = 0;
+warning_state = warning;
+warning('off') %#ok<*WNOFF>
+hnd_wait = waitbar(0,'Updating database...');
+
+for i=1:length(h)
+    try 
+        waitbar(i/length(h),hnd_wait,sprintf('Processing: %s ', [pth h(i).name]))
+    catch  %#ok<*CTCH>
+        waitbar(i/length(h),hnd_wait)
+    end
+
+    % Find the current file in the fileProcessProgressList
+    j = findFileInProgressList(h(i).name, filesProcessProgressList);
+    % if it doesn't exist add a new value
+    if j > length(filesProcessProgressList)
+        filesProcessProgressList(j).Name = h(i).name; %#ok<*AGROW>
+        filesProcessProgressList(j).Modified = 0;      % datenum(h(i).date);
+    end
+    % if the file modification data change since the last processing then
+    % reprocess it
+    if filesProcessProgressList(j).Modified < datenum(h(i).date)
+        try
+            % when a file is found that hasn't been processed try
+            % to load it. fr_read_EddyPro_file is able to read
+            % full_output, _biomet_ and EP-Summary files
+            fileName = fullfile(pth,h(i).name);
+            [~, ~,tv,Stats] = fr_read_EddyPro_file(fileName);
+            tv = tv + time_shift;
+            structType = 1;
+            db_struct2database(Stats,databasePath,0,[],timeUnit,missingPointValue,structType,1);         
+
+            % if there is no errors update records
+            numOfFilesProcessed = numOfFilesProcessed + 1;
+            numOfDataPointsProcessed = numOfDataPointsProcessed + length(tv);
+            filesProcessProgressList(j).Modified = datenum(h(i).date);
+
+        catch ME
+            fprintf(2,'\nError processing file: %s. \n',fileName);
+            fprintf(2,'%s\n',ME.message);
+            fprintf(2,'Error on line: %d in %s\n\n',ME.stack(1).line,ME.stack(1).file);
+        end % of try
+
+    end %  if filesProcessProgressList(j).Modified < datenum(h(i).date)
+end % for i=1:length(h)
+% Close progress bar
+close(hnd_wait)
+% Return warning state 
+try  %#ok<TRYNC>
+   for i = 1:length(warning_state)
+      warning(warning_state(i).identifier,warning_state(i).state)
+   end
+end
+
+save(processProgressListPath,'filesProcessProgressList')
+
+% this function returns and index pointing to where fileName is in the 
+% fileProcessProgressList.  If fileName doesn't exist in the list
+% the output is list length + 1
+function ind = findFileInProgressList(fileName, filesProcessProgressList)
+
+    ind = [];
+    for j = 1:length(filesProcessProgressList)
+        if strcmp(fileName,filesProcessProgressList(j).Name)
+            ind = j;
+            break
+        end %  if strcmp(fileName,filesProcessProgressList(j).Name)
+    end % for j = 1:length(filesProcessProgressList)
+    if isempty(ind)
+        ind = length(filesProcessProgressList)+1;
+    end 
--- matlab/New_eddy/fr_read_EddyPro_file.m
+++ matlab/New_eddy/fr_read_EddyPro_file.m
@@ -1,4 +1,4 @@
-function [EngUnits, Header,tv] = fr_read_EddyPro_file(fileName,assign_in,varName)
+function [EngUnits, Header,tv,outStruct] = fr_read_EddyPro_file(fileName,assign_in,varName)
 %  fr_read_EddyPro_file - reads EddyPro _full_output and _biomet_ files
 %
 % 
@@ -16,11 +16,18 @@ function [EngUnits, Header,tv] = fr_read_EddyPro_file(fileName,assign_in,varName
 %
 %
 % (c) Zoran Nesic                   File created:       Aug 25, 2022
-%                                   Last modification:  Jul 22, 2023
+%                                   Last modification:  Feb 14, 2024
 %
 
 % Revisions (last one first):
 %
+% Feb 14, 2024 (Zoran)
+%   - Big changes. Speed improvements in orders of magnitude.
+%       - Switched to using fr_read_generic_data_file.
+%       - improved flag creation
+%       - Added outStruct to the outputs.
+%   - This function now handles _full_output, _biomet_ AND EP_Summary files.
+%     This should make fr_read_SmartFlux_file obsolete.
 % Jul 22, 2023 (Zoran)
 %   - added modifications to be able to process _biomet_ files too.
 % Aug 30, 2022 (Zoran)
@@ -30,134 +37,62 @@ function [EngUnits, Header,tv] = fr_read_EddyPro_file(fileName,assign_in,varName
 %   - added spliting flag variables into multiple variables.
 %
 
-if contains(fileName,'_biomet_')
-    flagBiomet = 1;
-else
-    flagBiomet = 0;
-end
+    if contains(fileName,'_biomet_','IgnoreCase',true)
+        flagFileType = 'biomet';
+    elseif contains(fileName,'_full_output_','IgnoreCase',true)
+        flagFileType = 'fulloutput';
+    elseif contains(fileName,'_EP-Summary.txt','IgnoreCase',true)
+        flagFileType = 'summary';
+    else
+        error ('Wrong file type input to fr_read_EddyPro_file')
+    end
 
     try
-        % Set the defaults
-        arg_default('assign_in','base');
+        arg_default('assign_in',[]);
         arg_default('varName','Stats');
-
-        % read the first headearline 
-        fid = fopen(fileName);
-        tmp = textscan(fid,'%s',1,'delimiter','\n');
-        Header.line1 = char(tmp{1});
-        fclose(fid);
-
-        % read the second headearline
-        fid = fopen(fileName); 
-        tmp = textscan(fid,'%s',1,'delimiter','\n','headerlines',1);
-        Header.line2 = char(tmp{1});
-        fclose(fid);
-
-        if flagBiomet == 0
-            % read the third headearline
-            fid = fopen(fileName); 
-            tmp = textscan(fid,'%s',1,'delimiter','\n','headerlines',2);
-            Header.line3 = char(tmp{1});
-            fclose(fid);
-            varNamesLine = Header.line2;
-            unitsLine = Header.line3;
-            headerLinesN = 3;
-            skipFields = 4;
-            dateCol = 2;
-        else
-            varNamesLine = Header.line1;
-            unitsLine = Header.line2;
-            headerLinesN = 2;
-            skipFields = 3;
-            dateCol = 1;
-        end
-
-        % find how many fields are there
-        ind = strfind(double(varNamesLine),',');   % search for all tab delimeters (ascii = 9)
-        N = length(ind)+1;          % this is the number of fields
-
-        %Create: Header.var_names 
-        tmp = textscan(varNamesLine,'%s',N,'delimiter',','); % cell array of the var names
-        tmp = tmp{1};
-        for i=1:N-skipFields
-            newString = tmp{i+skipFields};
-            newString  = replace_string(newString,'-','_');
-            newString  = replace_string(newString,'u*','us');
-            newString  = strtrim(replace_string(newString,'(z_d)/L','zdL'));
-            newString  = replace_string(newString,'T*','ts');
-            newString  = replace_string(newString,'%','p');
-            newString  = replace_string(newString,'/','_');
-            Header.var_names{i} = newString;
-        end
-
-        %Create: Header.units 
-        tmp = textscan(unitsLine,'%s',N,'delimiter',','); % cell array of the var names
-        tmp = tmp{1};
-        for i=1:N-skipFields
-            Header.var_units{i} =char(tmp{i+skipFields}); %
-        end
-
-        % Create the data format string
-        if flagBiomet == 1
-            frmStr = '%s %s %s';  % "DATA file_name"
-        else
-            frmStr = '%s %s %s %s';  % "DATA file_name"
-        end
-        for i=1:N-skipFields
-            frmStr = [frmStr ' %f']; %#ok<*AGROW>
-        end
-        %frmStr = [frmStr '\n'];
-
-        % re-open the file and read the data
-        fid = fopen(fileName);
-        % read the data
-        EngUnits_tmp = textscan(fid,frmStr,'delimiter',',','headerlines',headerLinesN);
-        % Test for file inconsistancies (when something goes wrong the time vector will be corrupted)
-        timeVectorStr = strcat(char(EngUnits_tmp{dateCol}), ' '*ones(size(1,1)) ,char(EngUnits_tmp{dateCol+1}));
-        % Extract good time vectors and create idexes of good and bad rows
-        cntGood =  1;
-        indBad = [];
-        indGood = [];
-        for cntAll = 1:size(timeVectorStr,1)
-            try
-                tv(cntGood)=datenum(timeVectorStr(cntAll,:));
-                cntGood = cntGood+1;
-                indGood = [indGood cntAll];
-            catch
-                indBad = [indBad cntAll];
-            end
-        end
         
-        % store each tv(j) -> outStruct(j).TimeVector
-        for j=1:length(tv)                
-            outStruct(j,1).TimeVector = tv(j);    
-        end        
-        
-        % Reserve space only for good rows
-        EngUnits = NaN * ones(length(EngUnits_tmp{5})-length(indBad),N-skipFields);
-        % Convert to EngUnits
-        for i=1:N-skipFields
-            x = EngUnits_tmp{i+skipFields};
-            EngUnits(:,i) = x(indGood);
-            % It is possible to aks the program to output, in addition to the
-            % matrix EngUnits, all the variables and to put them in the callers
-            % space.
-            % store each x(j) -> outStruct(j).(var_name)
-            try
-                for j=1:length(x)                
-                    outStruct(j,1).(char(Header.var_names(i))) =x(j);  
-                end
-            catch ME
-                fprintf('**** ERROR ==>  ');
-                fprintf('%s\n',ME.message);
-                rethrow(ME)
-            end
+        if strcmpi(flagFileType,'fulloutput')
+            timeInputFormat = {[],'HH:mm'}; 
+            dateColumnNum = [2 3];
+            colToKeep = [4 Inf];
+            structType = 1;
+            inputFileType = 'delimitedtext';
+            modifyVarNames=0;
+            VariableNamesLine = 2;         
+            % [EngUnits, Header,tv,outStruct] = fr_read_generic_data_file(fileName,...
+            %                                                '',...
+            %                                                 [], [2 3],timeInputFormat,[4 Inf],1,'delimitedtext',0,2);           
+            [EngUnits,Header,tv,outStruct] = fr_read_generic_data_file(fileName,...
+                                                             [],[], dateColumnNum, timeInputFormat,colToKeep,structType,...
+                                                             inputFileType,modifyVarNames,VariableNamesLine);            
+        elseif strcmpi(flagFileType,'biomet')
+            timeInputFormat = {[],'HH:mm'}; 
+            dateColumnNum = [1 2];
+            colToKeep = [3 Inf];
+            structType = 1;
+            inputFileType = 'delimitedtext';
+            modifyVarNames=0;
+            VariableNamesLine = 1;         
+            [EngUnits,Header,tv,outStruct] = fr_read_generic_data_file(fileName,...
+                                                             [],[], dateColumnNum, timeInputFormat,colToKeep,structType,...
+                                                             inputFileType,modifyVarNames,VariableNamesLine);            
+            % [EngUnits, Header,tv,outStruct] = fr_read_generic_data_file(fileName,...
+            %                                                '',[], [1 2],timeInputFormat,[3 Inf],1,'delimitedtext',0,1);          
+        elseif strcmpi(flagFileType,'summary')
+            timeInputFormat = {[],'HH:mm:ss'};
+            dateColumnNum = [3 4];
+            colToKeep = [2 Inf];
+            structType = 1;
+            inputFileType = 'delimitedtext';
+            modifyVarNames=0;
+            VariableNamesLine = 1;
+            [EngUnits,Header,tv,outStruct] = fr_read_generic_data_file(fileName,...
+                                                             [],[], dateColumnNum, timeInputFormat,colToKeep,structType,...
+                                                             inputFileType,modifyVarNames,VariableNamesLine);
         end
 
-
-        
-        if flagBiomet == 0
-            % Extract individual flags from flag variables
+        % Extract individual flags from flag variables
+        if strcmpi(flagFileType,'summary') || strcmpi(flagFileType,'fulloutput')            
             flagVariables = {'spikes_hf','amplitude_resolution_hf','drop_out_hf',...
                              'absolute_limits_hf','skewness_kurtosis_hf','skewness_kurtosis_sf',...
                              'discontinuities_hf','discontinuities_sf','timelag_hf',...
@@ -165,32 +100,34 @@ end
             for j=1:size(outStruct,1)
                 for cntFlagsVars = 1:length(flagVariables)
                     flagField = char(flagVariables{cntFlagsVars});
-                    flagStr = sprintf('%d',outStruct(j,1).(flagField));
-                    for cntFlags = 1:length(flagStr)                
-                        flagNum = str2double(flagStr(cntFlags));
-                        if flagNum ==9 
-                            flagNum = -9999;
-                        end
-                        outStruct(j,1).(sprintf('%s_%d',flagField,cntFlags)) = flagNum;
+                    Ndigits = dec2base(outStruct(j,1).(flagField),10) - '0';
+                    for cntFlags = 1:size(Ndigits,2)                
+                        flagNum = Ndigits(:,cntFlags);
+                        flagNum(flagNum ==9) = -9999; 
+                        outStruct.(sprintf('%s_%d',flagField,cntFlags)) = flagNum;
                     end
                 end
             end
         end
-        
+
         if strcmpi(assign_in,'CALLER')
             assignin('caller',varName,outStruct);
         end
 
-        fclose(fid);
-
-    catch %#ok<CTCH>
-        fprintf('\nError reading file: %s. \n',fileName);
+    catch ME %#ok<CTCH>
+        fprintf(2,'\nError reading file: %s. \n',fileName);
+        fprintf(2,'%s\n',ME.message);
+        fprintf(2,'Error on line: %d in %s\n\n',ME.stack(1).line,ME.stack(1).file);
         EngUnits = [];
         Header = [];
         tv = [];
-        error 'Exiting function...'
     end       
 end
+
+
+
+
+
 %-------------------------------------------------------------------
 % function replace_string
 % replaces string findX with the string replaceX and padds
