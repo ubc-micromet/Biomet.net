5889d1f597bb05366a62918c18f76f5f9489abe4
znesic <z_nesic@hotmail.com>
2024-03-23 18:28:57 -0700

    recursive tag search in ta_get_index_tracceList now works

--- matlab/TRACEANALYSIS_FIRSTSTAGE/ta_get_index_traceList.m
+++ matlab/TRACEANALYSIS_FIRSTSTAGE/ta_get_index_traceList.m
@@ -27,13 +27,10 @@ end
 trc_names = trc_names(trc_names ~=32 & trc_names ~=9);
 
 % Split comma separated string trc_names into a structure:
-structTraceName = split(trc_names,',');
+structNamesOfDependants = split(trc_names,',');
 
 % Structure of all trace names
-structAllTraceNames = {list_of_traces.variableName};
-
-% Find if there are any tags (tag_*) in the list of dependents
-structAllTags = structTraceName(startsWith(structTraceName,'tag_'));
+structAllVariableNames = {list_of_traces.variableName};
 
 % Add custom tags from siteID_CustomTags.m file if such file exists
 % under Derived_Variables
@@ -41,57 +38,78 @@ structAllTags = structTraceName(startsWith(structTraceName,'tag_'));
 siteID = list_of_traces(1).SiteID;
 fileName = [siteID '_CustomTags'];
 if exist(fileName,'file')
-    customTags = eval(fileName);
-    customTagFieldNames = fieldnames(customTags);
+    customTags = eval(fileName);   
 else
-    customTagFieldNames = [];
+    customTags = [];
 end
 
-% if tags exist, convert them to trace names and add them
-% to the list of dependants
-if ~isempty(structAllTags)
-    indTaggedDependants = [];
-    for cntTags = 1:length(structAllTags)
-        cTag = char(structAllTags(cntTags));
-        indField = ismember(customTagFieldNames,cTag);
-        % Deal with special tags (tag_ALL, tag_AllMet, tag_AllFlux)        
-        if strcmpi(cTag,'tag_All')
-            % tag_All affects all traces
-            indTaggedDependants = 1:length(list_of_traces);
-        elseif indField ~= 0
-            % if cTag is memeber of customTags than add those trace names
-            structCustomTagTraces = strtrim(split(customTags.(char(customTagFieldNames(indField))),','))';
-            structTraceName = [structTraceName structCustomTagTraces]; %#ok<AGROW>
-        else
-            % loop through all the traces and find all the ones that have this tag
-            for cntTraces = 1:length(list_of_traces)
-                if isfield(list_of_traces(cntTraces).ini,'tag') && contains(list_of_traces(cntTraces).ini.tag,cTag)
-                    %fprintf('%d found tag\n',cntTraces);
-                    indTaggedDependants = [indTaggedDependants cntTraces];   %#ok<AGROW>
-                end
-            end
-        end
-    end
-
-    % remove tags from structTraceName 
-    structTraceName = structTraceName(~startsWith(structTraceName,'tag_'));
-    % and replace them with unique trace names
-    nDep = length(structTraceName);
-    for cnt = 1:length(indTaggedDependants)
-        structTraceName{cnt+nDep} = char(structAllTraceNames(indTaggedDependants(cnt)));
-    end
-    structTraceName = unique(structTraceName);
-
-end
-
-
+structNamesOfDependants = convert_tags_to_Traces(list_of_traces,structNamesOfDependants,customTags);
 
 % Get the indices of all traces:
-[~, ~, returnInd]=intersect(structTraceName,structAllTraceNames);
+[~, ~, returnInd]=intersect(structNamesOfDependants,structAllVariableNames);
 
 % make sure that returnInd is a row vector
 returnInd = returnInd(:)';
 
 
+function structNamesOfDependants = convert_tags_to_Traces(list_of_traces,structNamesOfDependants,customTags)
+    % It converts 
+    % Recursive search through all Tags to convert them to trace names 
+    
+    % Extract field names
+    if ~isempty(customTags)
+        customTagFieldNames = fieldnames(customTags);
+    else
+        customTagFieldNames = [];
+    end
 
+    % Structure of all trace names
+    structAllVariableNames = {list_of_traces.variableName};
 
+    % Find if there are any tags (tag_*) in the list of dependents
+    indAllTagsInDependents = startsWith(structNamesOfDependants,'tag_');
+    % move them to structAllTags
+    structAllTags = structNamesOfDependants(indAllTagsInDependents);
+    % and remove them from structNamesOfDependants
+    structNamesOfDependants = structNamesOfDependants(~indAllTagsInDependents);
+
+    % if tags exist, convert them to trace names and add them
+    % to the list of dependants
+    if ~isempty(structAllTags)
+        indTaggedDependants = [];
+        for cntTags = 1:length(structAllTags)
+            cTag = char(structAllTags(cntTags));
+            indField = find(ismember(customTagFieldNames,cTag));
+            if ~isempty(indField)
+                indField = indField(1);   % in case user made a mistake and there a same tag appears twice grab only the first one
+            end
+            % Deal with special tags (tag_ALL, tag_AllMet, tag_AllFlux)        
+            if strcmpi(cTag,'tag_All')
+                % tag_All affects all traces
+                indTaggedDependants = 1:length(list_of_traces);
+            elseif indField ~= 0
+                % if cTag is memeber of customTags than add those trace names
+                structCustomTagTraces = strtrim(split(customTags.(char(customTagFieldNames(indField))),','))';
+                structNamesOfDependants = [structNamesOfDependants structCustomTagTraces]; %#ok<AGROW>
+                % recursive call to check if there are more tag_ fields
+                structNamesOfDependants = convert_tags_to_Traces(list_of_traces,structNamesOfDependants,customTags);
+            else
+                % loop through all the traces and find all the ones that have this tag
+                for cntTraces = 1:length(list_of_traces)
+                    if isfield(list_of_traces(cntTraces).ini,'tag') && contains(list_of_traces(cntTraces).ini.tag,cTag)
+                        %fprintf('%d found tag\n',cntTraces);
+                        indTaggedDependants = [indTaggedDependants cntTraces];   %#ok<AGROW>
+                    end
+                end
+            end
+        end
+    
+        % remove tags from structTraceName 
+        structNamesOfDependants = structNamesOfDependants(~startsWith(structNamesOfDependants,'tag_'));
+        % and replace them with unique trace names
+        nDep = length(structNamesOfDependants);
+        for cnt = 1:length(indTaggedDependants)
+            structNamesOfDependants{cnt+nDep} = char(structAllVariableNames(indTaggedDependants(cnt)));
+        end
+        structNamesOfDependants = unique(structNamesOfDependants);
+    end
