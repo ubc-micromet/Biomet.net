71e374689c1724ced7ea209afaa44b8f780171f8
znesic <z_nesic@hotmail.com>
2024-04-05 15:26:19 -0700

    edits to tags_Standard

--- matlab/Micromet/getAllTags.m
+++ matlab/Micromet/getAllTags.m
@@ -4,7 +4,13 @@ function tagsOut = getAllTags(siteID)
 %
 %
 % Zoran Nesic               File created:           Apr 1, 2024
-%                           Last modification:      Apr 2, 2024
+%                           Last modification:      Apr 5, 2024
+
+% Revisions
+%
+% Apr 5, 2024 (Zoran)
+%  - improved handling of missing CustomTags
+%  - added beeps when the tag overwriting happens
 
 
 % first load the standard set
@@ -18,17 +24,22 @@ end
 fileName = [siteID '_CustomTags'];
 if exist(fileName,'file')
     tagsCustom = eval(fileName);
-else
-    tagsCustom = [];
-end
-allCustomFields = fieldnames(tagsCustom);
-allStandardFields = fieldnames(tagsOut);
-for cntFields = 1:length(allCustomFields)
-    fCustomName = char(allCustomFields(cntFields));
-    if ismember(fCustomName,allStandardFields)
-        fprintf(2,'Overwriting standard tag: %s with the same tag from %s.m\n',fCustomName,fileName);
+    allCustomFields = fieldnames(tagsCustom);
+    allStandardFields = fieldnames(tagsOut);
+    flagBeep = 1;
+    for cntFields = 1:length(allCustomFields)
+        fCustomName = char(allCustomFields(cntFields));
+        if ismember(fCustomName,allStandardFields)
+            if flagBeep == 1
+                beep
+                flagBeep  = 0;
+                fprintf('\n\n');
+            end
+            fprintf(2,'Overwriting standard tag: %s with the same tag from %s.m\n',fCustomName,fileName);
+        end
+        tagsOut.(fCustomName) = tagsCustom.(fCustomName);
     end
-    tagsOut.(fCustomName) = tagsCustom.(fCustomName);
 end
+
 
 
