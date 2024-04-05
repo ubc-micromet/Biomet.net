7e7e090aa69ccceb765b0646e3ccf164ad41b3b4
June-Skeeter <thruhikerflash@gmail.com>
2024-04-05 06:47:02 -0700

    Updated procedures to allow conditional execution on site-specific basis and streamlined writing of outputs

--- R/database_functions/ThirdStage.R
+++ R/database_functions/ThirdStage.R
@@ -17,7 +17,7 @@
 # source("C:/Biomet.net/R/database_functions/ThirdStage.R")
 
 # # Install on first run
-# install.packages(c('rs','yaml','rlist','dplyr','lubridate','data.table','tidyverse','caret'))
+# install.packages(c('rs','yaml','rlist','dplyr','lubridate','data.table','tidyverse','caret','ranger'))
 
 # Load libraries
 library('fs')
@@ -27,6 +27,44 @@ library("rlist")
 require("dplyr")
 require("lubridate")
 require("data.table")
+
+merge_nested_lists = function(...) {
+# Modified from: https://gist.github.com/joshbode/ed70291253a4b4412026
+  stack = rev(list(...))
+  names(stack) = rep('', length(stack))
+  result = list()
+
+  while (length(stack)) {
+    # pop a value from the stack
+    obj = stack[[1]]
+    root = names(stack)[[1]]
+    stack = stack[-1]
+
+    if (is.list(obj) && !is.null(names(obj))) {
+      if (any(names(obj) == '')) {
+        stop("Mixed named and unnamed elements are not supported.")
+      }
+
+      # restack for next-level processing
+      if (root != '') {
+        names(obj) = paste(root, names(obj), sep='|')
+      }
+      stack = append(obj, stack)
+    } else {
+      # clear a path to store result
+      path = unlist(strsplit(root, '|', fixed=TRUE))
+      for (j in seq_along(path)) {
+        sub_path = path[1:j]
+        if (is.null(result[[sub_path]])) {
+          result[[sub_path]] = list()
+        }
+      }
+      result[[path]] = obj
+    }
+  }
+
+  return(result)
+}
 
 configure <- function(siteID){
   # Get path of current script & arguments
@@ -56,20 +94,34 @@ configure <- function(siteID){
   # Read a the site specific configuration
   fn <- sprintf('%s_ThirdStage.yml',siteID)
   filename <- file.path(db_root,'Calculation_Procedures/TraceAnalysis_ini',siteID,fn)
-  config <- yaml.load_file(filename)
-  
+  site_config <- yaml.load_file(filename)
   # merge the config files
-  config <- c(dbase_config,config)
+  config <- merge_nested_lists(site_config,dbase_config)
   
   # Add the relevant paths
   config$Database$db_root <- db_root
   config$fx_path <- fx_path
   config$yrs <- yrs
+
+  # Set procedures to run by default unless specified otherwise in site-specific files
+  # Can update to have user overrides by command line as well if desired
+  # For now, just apply the overrides in site-specific config files
+  if(is.null(config$Processing$ThirdStage$Storage$Apply_Correction)){
+    config$Processing$ThirdStage$Storage$Apply_Correction=TRUE
+  }
+  if(is.null(config$Processing$ThirdStage$REddyProc$Run)){
+    config$Processing$ThirdStage$REddyProc$Run=TRUE
+  }
+  if(is.null(config$Processing$ThirdStage$RF_GapFilling$Run)){
+    config$Processing$ThirdStage$RF_GapFilling$Run=TRUE
+  }
   return(config)
 }
 
-read_traces <- function(){
+read_and_copy_traces <- function(){
   # Read function for loading data
+  # Read all traces from stage 2, copy to stage 3 and also dump to dataframe for stage 3 processing
+  # Any modified traces can be overwritten when dumping final stage 3 outputs
   sapply(list.files(pattern="read_database.R", path=config$fx_path, full.names=TRUE), source)
   
   siteID <- config$Metadata$siteID
@@ -77,16 +129,36 @@ read_traces <- function(){
   db_root <- config$Database$db_root
   data <- data.frame()
   
-  # Copy files from second stage to third stage, takes everything by default
-  # Can change behavior later if needed
+  # Copy files from second stage to third stage, copies everything by default  
   level_in <- config$Database$Paths$SecondStage
+  level_out <- config$Database$Paths$ThirdStage
+
   tv_input <- config$Database$Timestamp$name
   for (j in 1:length(yrs)) {
     in_path <- file.path(db_root,as.character(yrs[j]),siteID,level_in)
+    out_path <- file.path(db_root,as.character(yrs[j]),siteID,level_out)
+  
+    dir.create(out_path, showWarnings = FALSE)
+    unlink(file.path(out_path,'*'))
+
+    # First copy time-vector
+    file.copy(file.path(in_path,tv_input),
+              file.path(out_path,tv_input))
+
     copy_vars <- list.files(in_path)
     copy_vars <- copy_vars[! copy_vars %in% c(config$Metadata$tv_input)]
+    for (filename in copy_vars){
+      # Now copy traces
+      file.copy(file.path(in_path,filename),
+                file.path(out_path,filename))
+    }
     data.now <- read_database(db_root,yrs[j],siteID,level_in,copy_vars,tv_input,0)
     data <- dplyr::bind_rows(data,data.now)
+    # Save the config in the output folder (one copy per-year)
+    write_yaml(
+      config$Processing, 
+      file.path(out_path,'ProcessingSettings.yml'),
+      fileEncoding = "UTF-8")
   }
   # Create time variables
   data <- data %>%
@@ -105,28 +177,33 @@ read_traces <- function(){
   return(data)
 }
 
-storage_correction <- function(data_in){
-  Storage_Terms <- config$Processing$ThirdStage$Storage_Terms
+storage_correction <- function(){
+  Storage_Terms <- config$Processing$ThirdStage$Storage$Terms
   terms <- names(Storage_Terms)
-  for (term in terms){
-    flux <- names(Storage_Terms[[term]])
-    storage <- names(Storage_Terms[[term]])
-    if (flux %in% colnames(data_in)) {
-      data_in[[term]] <- data_in[[flux]]+data_in[[storage]]
-    }else{
-      print(sprintf('%s Not present in Second Stage, excluding from storage correction',flux))
-    }
+    for (term in terms){
+      flux <- names(Storage_Terms[[term]])
+      storage <- names(Storage_Terms[[term]])
+      # Default behavior is to apply correction 
+      if (flux %in% colnames(input_data) && config$Processing$ThirdStage$Storage$Apply_Correction) {
+        input_data[[term]] <- input_data[[flux]]+input_data[[storage]]
+      # If storage correction is set to false, still create the variable (eg NEE = FC) so it doesn't break anything
+      }else if (flux %in% colnames(input_data) && !config$Processing$ThirdStage$Storage$Apply_Correction) {
+        input_data[[term]] <- input_data[[flux]]
+      # Or notify user if not available
+      }else{
+        print(sprintf('%s Not present in Second Stage, excluding from storage correction',flux))
+      }
   }
-  return(data_in)
+  return(input_data)
 }
 
-ThirdStage_REddyProc <- function(data_in) {
+ThirdStage_REddyProc <- function() {
   
   # Subset just the config info relevant to REddyProc
   REddyConfig <- config$Processing$ThirdStage$REddyProc
   
-  # Limit to only variables present in data_in (e.g., exclude FCH4 if not present)
-  REddyConfig$vars_in <- lapply(REddyConfig$vars_in, function(x) if (x %in% colnames(data_in)){x})
+  # Limit to only variables present in input_data (e.g., exclude FCH4 if not present)
+  REddyConfig$vars_in <- lapply(REddyConfig$vars_in, function(x) if (x %in% colnames(input_data)){x})
   skip <- names(REddyConfig$vars_in[REddyConfig$vars_in=='NULL']) 
   for (var in skip){
     print(sprintf('%s Not present, REddyProc will not process',var))
@@ -134,7 +211,7 @@ ThirdStage_REddyProc <- function(data_in) {
   REddyConfig$vars_in <- REddyConfig$vars_in[!REddyConfig$vars_in=='NULL']
   
   # Rearrange data frame and only keep relevant variables for input into REddyProc
-  data_REddyProc <- data_in[ , c(unlist(REddyConfig$vars_in),"DateTime","Year","DoY","Hour")]
+  data_REddyProc <- input_data[ , c(unlist(REddyConfig$vars_in),"DateTime","Year","DoY","Hour")]
   # Rename column names to variable names in REddyProc
   colnames(data_REddyProc)<-c(names(REddyConfig$vars_in),"DateTime","Year","DoY","Hour")
   
@@ -184,53 +261,62 @@ ThirdStage_REddyProc <- function(data_in) {
   EProc$sGLFluxPartitionUStarScens()
   
   # Create data frame for REddyProc output
-  FilledEddyData <- EProc$sExportResults()
-  
+  REddyOutput <- EProc$sExportResults()
   # Delete uStar dulplicate columns since they are output for each gap-filled variables
-  vars_remove <- c(colnames(FilledEddyData)[grepl('\\Thres.', names(FilledEddyData))],
-                   colnames(FilledEddyData)[grepl('\\_fqc.', names(FilledEddyData))])
-  FilledEddyData <- FilledEddyData[, -which(names(FilledEddyData) %in% vars_remove)]
+  vars_remove <- c(colnames(REddyOutput)[grepl('\\Thres.', colnames(REddyOutput))],
+                   colnames(REddyOutput)[grepl('\\_fqc.', colnames(REddyOutput))])
+  if (length(vars_remove)>0){
+    REddyOutput <- REddyOutput[, -which(names(REddyOutput) %in% vars_remove)]
+  }
   
   # Revert to original input name (but maintain ReddyProc modifications that follow first underscore)
+  # Most are the same so doesn't matter, but some (e.g., Tair aren't standard AmeriFlux names)
   for (i in 1:length(REddyConfig$vars_in)){
     rep <- paste(as.character(names(REddyConfig$vars_in[i])),"_",sep="")
     sub <- paste(as.character(REddyConfig$vars_in[i]),"_",sep="")
-    uNames <- lapply(colnames(FilledEddyData), function(x) if (startsWith(x,rep)) {sub(rep,sub,x)} else {x})
-    colnames(FilledEddyData) <- uNames
+    uNames <- lapply(colnames(REddyOutput), function(x) if (startsWith(x,rep)) {sub(rep,sub,x)} else {x})
+    colnames(REddyOutput) <- uNames
   }
   
-  
-  FilledEddyData = dplyr::bind_cols(
-    data_in,FilledEddyData
+  REddyOutput = dplyr::bind_cols(
+    input_data[c("DateTime","Year","DoY","Hour")],REddyOutput
   )
-  
-  return(FilledEddyData)
+
+  # Write all REddyProc outputs to intermediate folder
+  # Update names for subset and save to main third stage folder
+  update_names <- config$Processing$ThirdStage$AmeriFlux_Names
+  input_data <- write_traces(REddyOutput,update_names)
+
+  return(input_data)
 }
 
-RF_GapFilling <- function(data){
+RF_GapFilling <- function(){
   
-  RFConfig <- config$Processing$ThirdStage$RF_GapFilling
+  RFConfig <- config$Processing$ThirdStage$RF_GapFilling$Models
   
   # Read function for RF gap-filling data
-  p <- sapply(list.files(pattern="RandomForestModelTest.R", path=config$fx_path, full.names=TRUE), source)
-  
+  p <- sapply(list.files(pattern="RandomForestModel.R", path=config$fx_path, full.names=TRUE), source)
+  # browser()
   # Check if dependent variable is available and run RF gap filling if it is
   for (fill_name in names(RFConfig)){
-    if (RFConfig[[fill_name]]$var_dep %in% colnames(data)){
+    if (RFConfig[[fill_name]]$var_dep %in% colnames(input_data)){
       var_dep <- unlist(RFConfig[[fill_name]]$var_dep)
       predictors <- unlist(strsplit(RFConfig[[fill_name]]$Predictors, split = ","))
       vars_in <- c(var_dep,predictors,"DateTime","DoY")
-      gap_filled <- RandomForestModel(
-        data[,vars_in],fill_name)
-      data = dplyr::bind_cols(data,gap_filled)
+      gap_filled <- RandomForestModel(input_data[,vars_in],fill_name)
+      gap_filled = dplyr::bind_cols(input_data[c("DateTime","Year","DoY","Hour")],gap_filled)
+      update_names <- list(fill_name)
+      names(update_names) <- c(fill_name)
+      input_data <- write_traces(gap_filled,update_names)
+
     }else{
-      print(sprintf('%s Not present, RandomForest will not process',RFConfig[[i]]$var_dep))
+      print(sprintf('%s Not present, RandomForest will not process',RFConfig[[fill_name]]$var_dep))
     }
   }
-  return(data)
+  return(input_data)
 }
 
-write_traces <- function(data){
+write_traces <- function(data,update_names){
   yrs <- config$yrs 
   siteID <- config$Metadata$siteID
   level_in <- config$Database$Paths$SecondStage
@@ -238,33 +324,24 @@ write_traces <- function(data){
   # Different output path for default vs advanced
   # This could create some ambiguity as to the source of final data
   if (config$Processing$ThirdStage$REddyProc$Ustar_filtering$run_defaults){
-    level_out <- config$Database$Paths$ThirdStage_Default
+    intermediate_out <- config$Database$Paths$ThirdStage_Default
   } else {
-    level_out <- config$Database$Paths$ThirdStage_Advanced
+    intermediate_out <- config$Database$Paths$ThirdStage_Advanced
   }
-  level_out_final <- config$Database$Paths$ThirdStage
-  copy_out_final <- config$Processing$ThirdStage$Final_Outputs
+  level_out <- config$Database$Paths$ThirdStage
   tv_input <- config$Database$datenum$filename
   db_root <- config$Database$db_root
   
   for (j in 1:length(yrs)){
-    # Create new directory, or clear existing directory (both intermediate and final)
+    # Create new directory, or clear existing directory
     dpath <- file.path(db_root,as.character(yrs[j]),siteID) 
+        
+    dir.create(file.path(dpath,intermediate_out), showWarnings = FALSE)
+    unlink(file.path(dpath,intermediate_out,'*'))
     
-    dir.create(file.path(dpath,level_out_final), showWarnings = FALSE)
-    unlink(file.path(dpath,level_out_final,'*'))
-    
-    dir.create(file.path(dpath,level_out), showWarnings = FALSE)
-    unlink(file.path(dpath,level_out,'*'))
-    
-    # Copy tv from stage 2 to 3 (intermediate and final) ... maybe not as "safe" as recalculating it from timestamp?
-    file.copy(
-      file.path(dpath,level_in,tv_input),
-      file.path(dpath,level_out,tv_input))
-    
-    file.copy(
-      file.path(dpath,level_in,tv_input),
-      file.path(dpath,level_out_final,tv_input))
+    # Copy tv from stage 2 to intermediate stage 3
+    file.copy(file.path(dpath,level_in,tv_input),
+              file.path(dpath,intermediate_out,tv_input))
     
     ind_s <- which(data$Year == yrs[j] & data$DoY == 1 & data$Hour == 0.5)
     ind_e <- which(data$Year == yrs[j]+1 & data$DoY == 1 & data$Hour == 0)
@@ -274,44 +351,57 @@ write_traces <- function(data){
     # Can parse down later as desired
     cols_out <- colnames(data)
     cols_out <- cols_out[! cols_out %in% c("DateTime","Year","DoY","Hour")]
-    setwd(file.path(dpath,level_out))
+
+    # Subset of traces can get appended to the input_data frame for use in subsequent steps if needed
+    append_cols <- data[unlist(update_names[update_names %in% cols_out])]
+    colnames(append_cols) <- names(update_names[update_names %in% cols_out])
+    
+    # Any columns to be added to input_data, that already exist within input data, will be overwritten with new incoming data to avoid confusion
+    input_data = dplyr::bind_cols(
+      input_data[!colnames(input_data) %in% colnames(append_cols)],
+      append_cols)
+
+    # Dump all data provided to intermediate output location
+    setwd(file.path(dpath,intermediate_out))
     for (i in 1:length(cols_out)){
       writeBin(as.numeric(data[ind,i]), cols_out[i], size = 4)
     }
     
     # Copy/rename final outputs
-    for (name in names(copy_out_final)){
-      if (file.exists(file.path(dpath,level_out,copy_out_final[name]))){
+    for (name in names(update_names)){
+      if (file.exists(file.path(dpath,intermediate_out,update_names[name]))){
         file.copy(
-          file.path(dpath,level_out,copy_out_final[name]),
-          file.path(dpath,level_out_final,name)
-        )
+          file.path(dpath,intermediate_out,update_names[name]),
+          file.path(dpath,level_out,name),
+          overwrite = TRUE)
       }else{
-        print(sprintf('%s was not created, cannot copy to final output for %i',copy_out_final[name],yrs[j]))
+        print(sprintf('%s was not created, cannot copy to final output for %i',update_names[name],yrs[j]))
       }            
     }
-    # Save the config in the output folder
-    write_yaml(
-      config$Processing, 
-      file.path(dpath,level_out_final,'ProcessingSettings.yml'),
-      fileEncoding = "UTF-8")
   } 
+  return(input_data)
 }
 
 start.time <- Sys.time()
 
+# Load configuration file
+config <- configure()
 
-config <- configure() # Load configuration file
+# Read Stage 2 Data
+input_data <- read_and_copy_traces() 
 
-input_data <- read_traces() # Read Stage 2 Data
+# Apply storage correction (if required)
+input_data <- storage_correction()
 
-input_data <- storage_correction(input_data)
+# Run REddyProc
+if (config$Processing$ThirdStage$REddyProc$Run){
+  input_data <- ThirdStage_REddyProc() 
+}
 
-FilledEddyData <- ThirdStage_REddyProc(input_data) # Run REddyProc
-
-FilledEddyData <- RF_GapFilling(FilledEddyData) # Run RF model
-
-write_traces(FilledEddyData) # Write Stage 3 Data
+# Run RF model
+if (config$Processing$ThirdStage$RF_GapFilling$Run){
+  input_data <- RF_GapFilling()
+} 
 
 end.time <- Sys.time()
 print('Stage 3 Complete, total run time:')
