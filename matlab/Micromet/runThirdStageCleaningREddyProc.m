a4031a5f0d74f714b67cf274e748211bee9b3b3f
znesic <z_nesic@hotmail.com>
2024-05-16 16:34:38 -0700

    Re-wrote runThirdStageCleaningREddyProc to enable it to work with the new ThirdStage.R function. Draft version.

--- matlab/Micromet/runThirdStageCleaningREddyProc.m
+++ matlab/Micromet/runThirdStageCleaningREddyProc.m
@@ -1,8 +1,8 @@
-function runThirdStageCleaningREddyProc(yearIn,siteID,Ustar_scenario,yearsToProcess,do_REddyProc)
+function runThirdStageCleaningREddyProc(yearIn,siteID,yearsToProcess)
 % runThirdStageCleaningREddyProc(yearIn,siteID)
 %
 % This function invokes Micromet third-stage cleaning R-script.
-% Usually, it's called from fr_automated_cleaning()
+% Usually, it's called by fr_automated_cleaning()
 %
 % Arguments
 %   yearIn          - year to clean
@@ -11,54 +11,35 @@ function runThirdStageCleaningREddyProc(yearIn,siteID,Ustar_scenario,yearsToProc
 %                     for gap-filling even when outputing one year only
 %
 % Zoran Nesic               File created:       Oct 25, 2022
-%                           Last modification:  Mar 20, 2024
+%                           Last modification:  May 16, 2024
 %
 
 % Revisions
 %
-% Mar 20, 2024 (Rosie)
-%   - Edited to run on both Windows PC and now also Mac OS (Ventura 13.5);
-%   - Other minor edits to remove warnings (Matlab 2023b).
-% Apr 28, 2023 (Zoran)
-%   - Added a new paramter to _setThirdStageCleaningParameters.R:
-%       -  fprintf(fidIni,'data_source    <- "%s"\n','MICROMET');
-% Feb 2, 2022 (Zoran/June)
-%   - fixed a bug in sub-function findRPath that incorrectly identified 
-%     the newest Rscript installation.
-% Nov 7, 2022 (Zoran)
-%  - Changed yearsToProcess from 1 to 99. Now all data for the site will be
-%    used for gap filling
-% Nov 6, 2022 (Zoran)
-%  - Added some comments
-% Nov 5, 2022 (Zoran)
-%  - changed the way ini data gets transfered to R. Instead of having a csv
-%    ini file siteID_ThirdStageCleaningParameters.ini, we'll use an 
-%    R script named: siteID '_setThirdStageCleaningParameters.R
-%    Much easier for the users.
-% Nov 2, 2022 (Zoran)
-%  - changed the name of main R script to Run_ThirdStage_REddyProc.R
-%  - arguments for R script are now passed mostly as data in an ini
-%    file: siteID_ThirdStageCleaningParameters.ini. Only the path to
-%    the ini file is passed directly as an input argument to Run_ThirdStage_REddyProc.R
+% May 16, 2024 (Zoran)
+%   - Major change to match the new R function ThirdStage.R. 
+%   - Different input parameters.
+%   - Moved the old revision notes to the bottom of the file because
+%     they are less relevant now after this update
 
-    
     % Prepare input parameters, create defaults if needed
-    arg_default('Ustar_scenario','fast')  % use fast processing (other option is 'full')
-    arg_default('yearsToProcess',99);     % Use all years since the site was established for gap filling
-    arg_default('do_REddyProc',1);
-
+    arg_default('yearsToProcess',1);     % Use only the current year for gap filling
+    
+    startYear = yearIn - yearsToProcess + 1;
+    endYear   = yearIn;
 
     % find path to R database_functions folder
     pthBiometR = findBiometRpath;
-    pthBiometR(strfind(pthBiometR,"\"))="/";  % Convert the separator to R's prefered.
+    %pthBiometR(strfind(pthBiometR,"\"))="/";  % Convert the separator to R's prefered.
+    pthThirdStageR = fullfile(pthBiometR,"ThirdStage.R");
     
     % find path to database
     pthDatabase = findDatabasePath;
-    pthDatabase(strfind(pthDatabase,"\"))="/"; % Convert the separator to R's prefered.
+    % pthDatabase(strfind(pthDatabase,"\"))="/"; % Convert the separator to R's prefered.
     
     % This one should be under ../Database/Calculation_procedures\TraceAnalysis_ini\siteID:
     pthIni = fullfile(pthDatabase,'Calculation_Procedures','TraceAnalysis_ini',siteID);
-    pthIni(strfind(pthIni,"\"))="/";       % Convert the separator to R's prefered.
+    % pthIni(strfind(pthIni,"\"))="/";       % Convert the separator to R's prefered.
     
     % Find path to the newest version of Rscript
     pthRbin = findRPath;    
@@ -69,95 +50,109 @@ function runThirdStageCleaningREddyProc(yearIn,siteID,Ustar_scenario,yearsToProc
         mkdir(fullfile(pthIni,'log'))
     end    
     pthLogFile = fullfile(pthIni,'log',[siteID '_ThirdStageCleaning.log']);
-    pthLogFile(strfind(pthLogFile,"\"))="/";       % Convert the separator to R's prefered.
-    
-    % the *.bat or *.sh file that runs Rscript also goes into the log folder 
-    if ispc
-        pthBatchFile = fullfile(pthIni,'log',[siteID '_ThirdStageCleaning.bat']);
-    elseif ismac
-        pthBatchFile = fullfile(pthIni,'log',[siteID '_ThirdStageCleaning.sh']);
-    end
-
-    % The cleaning script Run_REddyProc_ThirdStage_siteID.R is under ../Biomet.net/R
-    pthCleaningScript = fullfile(pthBiometR,'Run_ThirdStage_REddyProc.R');
-    pthCleaningScript(strfind(pthCleaningScript,"\"))="/";       % Convert the separator to R's prefered.
-    
-    
-    % Create an input file for StageThreeREddyProc() function and pass 
-    % all the arguments to it. Store that file with .bat and .log files:
-    pthSetClParam = fullfile(pthIni,'log',[siteID '_setThirdStageCleaningParameters.R']);
-    
-    % Get the function stack (it will be needed to extract this function's name)
-    st=dbstack;
-    
-    % Store the ini parameters in pthSetClParam
-    fidIni = fopen(pthSetClParam,'w');
-    if fidIni <0
-        error('Could not open %s file!\n',pthSetClParam);
-    end
-    fprintf(fidIni,'#-------------------------------------------------------------\n');
-    fprintf(fidIni,'# This file is automatically generated by the Matlab function:\n');
-    fprintf(fidIni,'# %s.m\n',st(1).name);
-    fprintf(fidIni,'# %s\n',datestr(now)); %#ok<TNOW1,DATST>
-    fprintf(fidIni,'#--------------------------------------------------------------\n');
-    fprintf(fidIni,'data_source    <- "%s"\n','MICROMET');
-    fprintf(fidIni,'site    <- "%s"\n',siteID);
-    fprintf(fidIni,'yrs     <- %d\n',yearIn);
-    fprintf(fidIni,'db_root <- "%s"\n',pthDatabase);
-    fprintf(fidIni,'fx_path <- "%s"\n',pthBiometR);
-    fprintf(fidIni,'Ustar_scenario <- "%s"\n',Ustar_scenario);
-    fprintf(fidIni,'do_REddyProc   <- %d\n',do_REddyProc);
-    fprintf(fidIni,'yearsToProcess <- %d\n',yearsToProcess);
-    fprintf(fidIni,'pthDatabase    <- "%s"\n',pthDatabase);
-    fprintf(fidIni,'pthBiometR     <- "%s"\n',pthBiometR);
-    fprintf(fidIni,'pthLogFile     <- "%s"\n',pthLogFile);
-    fprintf(fidIni,'ini_path       <- "%s"\n',pthIni);
-    fclose(fidIni);
-    
-    
-    % create the batch file command line 
-    if ispc
-        cmdLine = ['"' pthRbin '" "'  pthCleaningScript '" "' pthBiometR '" "' pthSetClParam '" '...
-               ' 2> "' pthLogFile '" 1>&2'];
-    elseif ismac
-        cmdLine = [ pthRbin  ' '  pthCleaningScript ' ' pthBiometR ' ' pthSetClParam ' '...
-               ' 2> ' pthLogFile ' 1>&2'];
-    end
+    % pthLogFile(strfind(pthLogFile,"\"))="/";       % Convert the separator to R's prefered.
+    % 
+    % % the *.bat or *.sh file that runs Rscript also goes into the log folder 
+    % if ispc
+    %     pthBatchFile = fullfile(pthIni,'log',[siteID '_ThirdStageCleaning.bat']);
+    % elseif ismac
+    %     pthBatchFile = fullfile(pthIni,'log',[siteID '_ThirdStageCleaning.sh']);
+    % end
+    % 
+    % % The cleaning script Run_REddyProc_ThirdStage_siteID.R is under ../Biomet.net/R
+    % pthCleaningScript = fullfile(pthBiometR,'Run_ThirdStage_REddyProc.R');
+    % pthCleaningScript(strfind(pthCleaningScript,"\"))="/";       % Convert the separator to R's prefered.
+    % 
+    % 
+    % % Create an input file for StageThreeREddyProc() function and pass 
+    % % all the arguments to it. Store that file with .bat and .log files:
+    % pthSetClParam = fullfile(pthIni,'log',[siteID '_setThirdStageCleaningParameters.R']);
+    % 
+    % % Get the function stack (it will be needed to extract this function's name)
+    % st=dbstack;
+    % 
+    % % Store the ini parameters in pthSetClParam
+    % fidIni = fopen(pthSetClParam,'w');
+    % if fidIni <0
+    %     error('Could not open %s file!\n',pthSetClParam);
+    % end
+    % fprintf(fidIni,'#-------------------------------------------------------------\n');
+    % fprintf(fidIni,'# This file is automatically generated by the Matlab function:\n');
+    % fprintf(fidIni,'# %s.m\n',st(1).name);
+    % fprintf(fidIni,'# %s\n',datestr(now)); %#ok<TNOW1,DATST>
+    % fprintf(fidIni,'#--------------------------------------------------------------\n');
+    % fprintf(fidIni,'data_source    <- "%s"\n','MICROMET');
+    % fprintf(fidIni,'site    <- "%s"\n',siteID);
+    % fprintf(fidIni,'yrs     <- %d\n',yearIn);
+    % fprintf(fidIni,'db_root <- "%s"\n',pthDatabase);
+    % fprintf(fidIni,'fx_path <- "%s"\n',pthBiometR);
+    % fprintf(fidIni,'Ustar_scenario <- "%s"\n',Ustar_scenario);
+    % fprintf(fidIni,'do_REddyProc   <- %d\n',do_REddyProc);
+    % fprintf(fidIni,'yearsToProcess <- %d\n',yearsToProcess);
+    % fprintf(fidIni,'pthDatabase    <- "%s"\n',pthDatabase);
+    % fprintf(fidIni,'pthBiometR     <- "%s"\n',pthBiometR);
+    % fprintf(fidIni,'pthLogFile     <- "%s"\n',pthLogFile);
+    % fprintf(fidIni,'ini_path       <- "%s"\n',pthIni);
+    % fclose(fidIni);
+    % 
+    % 
+    % % create the batch file command line 
+    % if ispc
+    %     cmdLine = ['"' pthRbin '" "'  pthCleaningScript '" "' pthBiometR '" "' pthSetClParam '" '...
+    %            ' 2> "' pthLogFile '" 1>&2'];
+    % elseif ismac
+    %     cmdLine = [ pthRbin  ' '  pthCleaningScript ' ' pthBiometR ' ' pthSetClParam ' '...
+    %            ' 2> ' pthLogFile ' 1>&2'];
+    % end
     
     tic;
     tv_start = now; %#ok<TNOW1>
-    % Create the batch file that calls R (or shell script for Mac OS)
-    fidBatch = fopen(pthBatchFile,'w');
-    if fidBatch <0
-        error('Could not open %s file!\n',fidBatch);
-    end
-    if ispc
-        fprintf(fidBatch,'REM ----------------------------------------------------------\n');
-        fprintf(fidBatch,'REM  This file is automatically generated by Matlab function:\n');
-        fprintf(fidBatch,'REM  %s.m\n',st(1).name);
-        fprintf(fidBatch,'REM  %s\n',datestr(now)); %#ok<TNOW1,DATST>
-        fprintf(fidBatch,'REM ----------------------------------------------------------\n');
-    elseif ismac
-        fprintf(fidBatch,'# ----------------------------------------------------------\n');
-        fprintf(fidBatch,'#  This file is automatically generated by Matlab function:\n');
-        fprintf(fidBatch,'#  %s.m\n',st(1).name);
-        fprintf(fidBatch,'#  %s\n',datestr(now)); %#ok<TNOW1,DATST>
-        fprintf(fidBatch,'# ----------------------------------------------------------\n');
-    end
-    fprintf(fidBatch,'%s\n',cmdLine);
-    fclose(fidBatch);
-    if ispc
-        [~,cmdOutput] = system(pthBatchFile); %#ok<ASGLU>
-    elseif ismac
-        % add permissions to execute new file
-        system(['chmod +x ' pthBatchFile]);
-        [~,cmdOutput] = system(['bash ' pthBatchFile]); %#ok<ASGLU>
-    end
-    
+    % % Create the batch file that calls R (or shell script for Mac OS)
+    % fidBatch = fopen(pthBatchFile,'w');
+    % if fidBatch <0
+    %     error('Could not open %s file!\n',fidBatch);
+    % end
+    % if ispc
+    %     fprintf(fidBatch,'REM ----------------------------------------------------------\n');
+    %     fprintf(fidBatch,'REM  This file is automatically generated by Matlab function:\n');
+    %     fprintf(fidBatch,'REM  %s.m\n',st(1).name);
+    %     fprintf(fidBatch,'REM  %s\n',datestr(now)); %#ok<TNOW1,DATST>
+    %     fprintf(fidBatch,'REM ----------------------------------------------------------\n');
+    % elseif ismac
+    %     fprintf(fidBatch,'# ----------------------------------------------------------\n');
+    %     fprintf(fidBatch,'#  This file is automatically generated by Matlab function:\n');
+    %     fprintf(fidBatch,'#  %s.m\n',st(1).name);
+    %     fprintf(fidBatch,'#  %s\n',datestr(now)); %#ok<TNOW1,DATST>
+    %     fprintf(fidBatch,'# ----------------------------------------------------------\n');
+    % end
+    % fprintf(fidBatch,'%s\n',cmdLine);
+    % fclose(fidBatch);
+    % if ispc
+    %     [~,cmdOutput] = system(pthBatchFile); %#ok<ASGLU>
+    % elseif ismac
+    %     % add permissions to execute new file
+    %     system(['chmod +x ' pthBatchFile]);
+    %     [~,cmdOutput] = system(['bash ' pthBatchFile]); %#ok<ASGLU>
+    % end
 
-    % When R is finished, print the footer in the log file
-    fidLog = fopen(pthLogFile,'a');      
+    % Run RScript
+    % concatenate the command line argument
+    CLI_args = sprintf('"%s" --vanilla %s %s %i %i',pthRbin,pthThirdStageR ,siteID,startYear,endYear);
+    % run the command line argument
+    [statusR,cmdOutput] = system(CLI_args,'-echo');
+
+    % When R is finished, print cmdOutput and the footer in the log file
+    fidLog = fopen(pthLogFile,'w');      
     if fidLog > 0    
+        fprintf(fidLog,'=============================================================\n');
+        if statusR == 0
+            fprintf(fidLog,'Completed running Third Stage Rscript: %s\n',pthRbin);
+        else
+            fprintf(fidLog,'Failed running Third Stage Rscript: %s\n',pthRbin);
+        end
+        fprintf(fidLog,'=============================================================\n');
+        fprintf(fidLog,'\n\nCommand output:\n\n');
+        fprintf(fidLog,cmdOutput);
         fprintf(fidLog,'\n\n\n\n');
         fprintf(fidLog,'=============================================================\n');
         fprintf(fidLog,'Rscript:      %s\n',pthRbin);
@@ -169,6 +164,12 @@ function runThirdStageCleaningREddyProc(yearIn,siteID,Ustar_scenario,yearsToProc
     end
     
 end
+
+
+
+% ===============================================================================================
+% Local functions
+%================================================================================================
 
 function biometRpath = findBiometRpath
     funA = which('read_bor');     % First find the path to Biomet.net by looking for a standard Biomet.net functions
@@ -223,3 +224,28 @@ function Rpath = findRPath
 end
         
 
+% OLD revisions
+% Mar 20, 2024 (Rosie)
+%   - Edited to run on both Windows PC and now also Mac OS (Ventura 13.5);
+%   - Other minor edits to remove warnings (Matlab 2023b).
+% Apr 28, 2023 (Zoran)
+%   - Added a new paramter to _setThirdStageCleaningParameters.R:
+%       -  fprintf(fidIni,'data_source    <- "%s"\n','MICROMET');
+% Feb 2, 2022 (Zoran/June)
+%   - fixed a bug in sub-function findRPath that incorrectly identified 
+%     the newest Rscript installation.
+% Nov 7, 2022 (Zoran)
+%  - Changed yearsToProcess from 1 to 99. Now all data for the site will be
+%    used for gap filling
+% Nov 6, 2022 (Zoran)
+%  - Added some comments
+% Nov 5, 2022 (Zoran)
+%  - changed the way ini data gets transfered to R. Instead of having a csv
+%    ini file siteID_ThirdStageCleaningParameters.ini, we'll use an 
+%    R script named: siteID '_setThirdStageCleaningParameters.R
+%    Much easier for the users.
+% Nov 2, 2022 (Zoran)
+%  - changed the name of main R script to Run_ThirdStage_REddyProc.R
+%  - arguments for R script are now passed mostly as data in an ini
+%    file: siteID_ThirdStageCleaningParameters.ini. Only the path to
+%    the ini file is passed directly as an input argument to Run_ThirdStage_REddyProc.R
