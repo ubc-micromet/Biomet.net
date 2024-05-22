bf52daaf45ecfa6faaa2f8efb3d6fc657f929f35
znesic <z_nesic@hotmail.com>
2024-05-22 18:38:53 -0500

    removed Biomet.net copy of startup.m. Prevents weird bugs when Matlab is not setup properly. This way, a good copy of startup.m has to be on the user path for Biomet.net to work.

--- matlab/Micromet/set_TAB_project.m
+++ matlab/Micromet/set_TAB_project.m
@@ -13,6 +13,9 @@ function structProject = set_TAB_project(projectPath,flagSetUserData)
 %
 
 arg_default('flagSetUserData',0);
+
+% Make sure the file separators are set properly for 
+% Windows and macOS
 projectPath = fullfile(projectPath);
 
 % projectPath must exist
@@ -20,12 +23,13 @@ if ~exist(projectPath,'dir')
     error('Folder: %s does not exist!',projectPath);
 end
 
+% set the Matlab current path to projectPath
+cd(projectPath)
+
 % ----------------------------------------
 % load yaml file into structProject here
 % ----------------------------------------
-structProject = pretendYMLread(projectPath);
-% readingYaml 
-%yaml.dump(structProject)
+structProject = pretend_configYAML(projectPath);
 
 % if required, save data under UserData so it's visible 
 % to all Matlab functions.
@@ -35,45 +39,9 @@ if flagSetUserData == 1
     set(0,"UserData",UserData)
 end
 
-
-function structProject = pretendYMLread(projectPath)
-
-% Write a Matlab GUI to create the initial YAML than can then be
-% edited by either the GUI or by the user
-%-------- start yaml ---------------------
-projectName = 'UQAM';
-structProject.projectName   = projectName;
-structProject.path          = fullfile(projectPath,structProject.projectName);
-structProject.databasePath  = fullfile(structProject.path,'Database');
-structProject.sitesPath     = fullfile(structProject.path,'Sites');
-structProject.matlabPath    = fullfile(structProject.path,'Matlab');
-
-siteID = 'UQAM_0';
-structProject.sites.(siteID).siteID = siteID;
-
-% Data logger tables
-tableNum = 1;
-structProject.sites.(siteID).dataSources.met.table(tableNum).name              = 'Met_30m';
-structProject.sites.(siteID).dataSources.met.table(tableNum).source            = [siteID '_' structProject.sites.(siteID).dataSources.met.table(tableNum).name];
-structProject.sites.(siteID).dataSources.met.table(tableNum).timeStepMin       = 30;
-structProject.sites.(siteID).dataSources.met.table(tableNum).dbFolderName      = 'Met';
-tableNum = tableNum + 1;
-structProject.sites.(siteID).dataSources.met.table(tableNum).name              = 'Met_05m';
-structProject.sites.(siteID).dataSources.met.table(tableNum).source            = [siteID '_' structProject.sites.(siteID).dataSources.met.table(tableNum).name];
-structProject.sites.(siteID).dataSources.met.table(tableNum).timeStepMin       = 5;
-structProject.sites.(siteID).dataSources.met.table(tableNum).dbFolderName      = fullfile('Met',structProject.sites.(siteID).dataSources.met.table(tableNum).name);
-tableNum = tableNum + 1;
-structProject.sites.(siteID).dataSources.met.table(tableNum).name              = 'RawData_05m';
-structProject.sites.(siteID).dataSources.met.table(tableNum).source            = [siteID '_' structProject.sites.(siteID).dataSources.met.table(tableNum).name];
-structProject.sites.(siteID).dataSources.met.table(tableNum).timeStepMin       = 5;
-structProject.sites.(siteID).dataSources.met.table(tableNum).dbFolderName      = fullfile('Met',structProject.sites.(siteID).dataSources.met.table(tableNum).name);
-
-% ECCC stations
-structProject.sites.(siteID).dataSources.eccc(1).stationsID                    = 27646;
-structProject.sites.(siteID).dataSources.eccc(1).stationsName                  = 'SHAWINIGAN';
-structProject.sites.(siteID).dataSources.eccc(2).stationsID                    = 8321;
-structProject.sites.(siteID).dataSources.eccc(2).stationsName                  = 'TROIS-RIVIERES';
-
-
-%-------- end yaml ---------------------------
+% to keep compatibility with the legacy code, this function
+% will now create the following Matlab functions in the projectPath folder:
+% - biomet_database_default.m
+% - biomet_sites_default.m
+% and then setup the 
 
