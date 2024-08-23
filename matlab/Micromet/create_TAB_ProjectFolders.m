c20b9faa4ea311be907938068b39fed41d1f6c19
znesic <z_nesic@hotmail.com>
2024-08-23 16:31:27 -0700

    New function: create_TAB_ProjectFolders(projectPath,siteID) that creates a Projects folder structure and clones some required files from GitHub. It's something that a new user would use when they first start using our data pipeline.

--- matlab/Micromet/create_TAB_ProjectFolders.m
+++ matlab/Micromet/create_TAB_ProjectFolders.m
@@ -1,33 +1,103 @@
-function create_TAB_ProjectFolders(projectPath)
+function create_TAB_ProjectFolders(projectPath,siteID)
+% Create the directory structure for Trace Analysis Biomet (TAB) data projects
+% create_TAB_ProjectFolders ('E:\Projects\My_MicrometSites','Site_1')
+%                           - creates the data tree to be used with TAB projects
+%                           - clones the common set of files from GigHub
+%                           - follows the instructions given here: 
+%                              (Point to pipeline-documentation GigHum)
+%
+%
+% Inputs:
+%   projectPath         - path to the main project folder (parent of Database and Sites folders)
+%   siteID              - short Ameriflux site ID or your site ID. No spaces are allowed.
+%
+% Output:
+%
+%
+% Zoran Nesic           File created:       Aug 23, 2024
+%                       Last modification:  Aug 23, 2024
 
-if ~exist(projectPath,'var')
-    error('Missing required input.')
+% Revisions
+%
+
+if ~exist('projectPath','var')
+    error('Missing Project Path.')
+end
+if ~exist('siteID','var')
+    error('Missing siteID.')
+else
+    siteID = upper(siteID);
 end
 
-if ~exist("projectPath",'dir')
+if ~exist(projectPath,'dir')
     mkdir(projectPath);
 end
 
 if exist(fullfile(projectPath,'Database'),'dir') ...
-   | exist(fullfile(projectPath,'Sites'),'dir') ...
-   | exist(fullfile(projectPath,'Matlab'),'dir')
+   || exist(fullfile(projectPath,'Sites'),'dir') ...
+   || exist(fullfile(projectPath,'Matlab'),'dir')
     fig = uifigure;
     selection = uiconfirm(fig, ...
     "Project folders already exist. Do you want to overwrite them?","Confirm Overwrite", ...
     "Options",["Overwrite","Cancel"], ...
-    "Icon","warning");
+    "Icon","warning",'CloseFcn',@(h,e) close(fig));
     if strcmpi(selection,'Cancel')
         return
     end
 end
-mkdir(fullfile(projectPath,'Database'));
-mkdir(fullfile(projectPath,'Database','Calculation_Procedures'));
-pathAF = fullfile(projectPath,'Database','Calculation_Procedures','AmeriFLux');
-mkdir(pathAF);
+
+tmpPath = fullfile(projectPath,'Database');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures','AmeriFlux');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+else
+    % For repo to be downloaded the folder needs to be empty so first remove it
+    rmdir(tmpPath,"s");
+    mkdir(tmpPath);
+end
 url1 = 'https://github.com/CANFLUX/TAB_Ameriflux_variables.git';
+gitclone(url1,tmpPath,Depth=1);
 
-repo = gitclone(url1,pathAF,Depth=1);
-mkdir(fullfile(projectPath,'Database','Calculation_Procedures','TraceAnalysis_ini'));
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures','TraceAnalysis_ini');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures','TraceAnalysis_ini',siteID);
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures','TraceAnalysis_ini',siteID,'Derived_Variables');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Database','Calculation_Procedures','TraceAnalysis_ini',siteID,'log');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Sites');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
+
+tmpPath = fullfile(projectPath,'Sites',siteID);
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
 
 
-mkdir(fullfile(projectPath,'Sites'));
+tmpPath = fullfile(projectPath,'Matlab');
+if ~exist(tmpPath,'dir')
+    mkdir(tmpPath);
+end
